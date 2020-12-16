f95063d0fbb534428970ae661b5628d9fe7c02b9
Alexander Fasching <fasching.a91@gmail.com>
2020-12-16 11:56:38 +0100

    Add platform for drone project

---
+++ src/plat/drone-soc/config.cmake
@@ -0,0 +1,22 @@
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(drone-soc KernelPlatformDroneSOC PLAT_DRONE_SOC KernelArchRiscV)
+
+if(KernelPlatformDroneSOC)
+    declare_seL4_arch(riscv64)
+    config_set(KernelRiscVPlatform RISCV_PLAT "drone-soc")
+    config_set(KernelPlatformFirstHartID FIRST_HART_ID 0)
+    list(APPEND KernelDTSList "tools/dts/drone-soc.dts")
+    declare_default_headers(
+        TIMER_FREQUENCY 20000000llu PLIC_MAX_NUM_INT 0
+        INTERRUPT_CONTROLLER arch/machine/plic.h
+    )
+else()
+    unset(KernelPlatformFirstHartID CACHE)
+endif()
